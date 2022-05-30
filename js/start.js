f754982efc616aeb39cdd6bdedc7055ab5afb8ac
baegyeong <beth0929@naver.com>
2022-05-30 12:58:50 +0900

    디렉토리 수정

--- js/start.js
+++ js/start.js
@@ -1,4 +1,4 @@
 function clickBg() {
-  window.location.href = "C:/Users/User/Desktop/uhsa/index2.html";
+  window.location.href = "C:/Users/User/Desktop/uhsa/html/index2.html";
 }
 window.addEventListener("click", clickBg);
