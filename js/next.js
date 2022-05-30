0627306be17b16650a6534d914de4feb46b5f8f2
baegyeong <beth0929@naver.com>
2022-05-30 11:30:16 +0900

    second commit

---
+++ js/next.js
@@ -0,0 +1,6 @@
+next = document.getElementById("next");
+
+function clickBtn() {
+  window.location.href = "C:/Users/User/Desktop/uhsa/html/choice.html";
+}
+next.addEventListener("click", clickBtn);
