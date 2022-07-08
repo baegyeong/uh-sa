d0d6180613976f9e99838ab4288d5692891cbea4
baegyeong <beth0929@naver.com>
2022-07-08 20:22:06 +0900

    stop 버튼 수정

--- people_quiz/timer.js
+++ people_quiz/timer.js
@@ -1,29 +1,38 @@
-const name = document.querySelector(".name");
-
-let answer = setTimeout(function () {
-  name.style.display = "flex";
-}, 3000);
-
 const timer = document.querySelector("#Timer");
+const name = document.querySelector(".name");
+const Stop = document.querySelector(".stop");
 
+// 타이머 설정 및 0초 시 정답 띄우기
 let time = 2;
 let sec = "";
 timer.innerText = "00" + ":0" + "3";
-let setTime = setInterval(function () {
+
+let goTimeZero = function () {
   sec = time;
   timer.innerText = "00" + ":0" + sec;
   time--;
-}, 1000);
+  if (time === -1) {
+    name.style.display = "flex";
+    clearInterval(setTime);
+  }
+};
 
-setTimeout(function () {
-  clearInterval(setTime);
-}, 3000);
+let setTime = setInterval(goTimeZero, 1000);
 
-const Stop = document.querySelector(".stop");
-
+// stop 버튼을 통해 타이머, 정답화면 제어
 function clickBtn(event) {
-  event.preventDefault();
-  clearInterval(setTime);
-  clearInterval(answer);
+  if (Stop.innerText === "STOP") {
+    event.preventDefault();
+    clearInterval(setTime);
+    Stop.innerText = "PLAY";
+    Stop.style.color = "#008bff";
+  } else {
+    console.log(sec);
+    setTime = setInterval(goTimeZero, 1000);
+
+    Stop.innerText = "STOP";
+    Stop.style.color = "red";
+  }
 }
+
 Stop.addEventListener("click", clickBtn);
